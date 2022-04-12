c2a974b2e807dc31ec83a52ec31394f17f45ed06
JBayne <61173594+JBayne07@users.noreply.github.com>
2022-04-12 18:50:19 -0400

    feat:Implements start session button, and end session button. This allows the user to click start session, and will immediately deplete the battery. The user can also click end session, and will stop the session running and stop the battery from being depleted.

--- CES_Device_Simulator/Battery.cpp
+++ CES_Device_Simulator/Battery.cpp
@@ -5,6 +5,14 @@ Battery::Battery(int n) : powerLevel(n)
 
 }
 
+int Battery::getPowerLevel(){
+    return powerLevel;
+}
+
+void Battery::setPowerLevel(int value){
+    powerLevel = value;
+}
+
 int Battery::decrement(int n)
 {
     powerLevel -= n;
